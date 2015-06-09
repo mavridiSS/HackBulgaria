00b0440de8622432c6c3d4fa9e03f46a5e9ae1ed
Georgi Mavridis <georgimavridis21@gmail.com>
2015-06-10 01:38:45 +0300

    folder renaming

--- week7/SQL-Starter/polyglotDBscript.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("polyglot.db")
-cursor = conn.cursor()
-
-result = cursor.execute("SELECT id, language FROM languages")
-
-for row in result:
-    print(row)
