81a8f0ce6dcc7d3eb1c69798fa852cc4f5172723
Georgi Mavridis <georgimavridis21@gmail.com>
2015-06-10 01:44:06 +0300

    folder renaming

--- week8/Cinema-Reservation-System/settings.py
+++
@@ -1,7 +0,0 @@
-DB_SQL_FILE = "create_cinema_database.sql"
-DB_NAME = "cinema.db"
-
-ROWS = 10
-COLUMNS = 10
-FREE_SEAT = '.'
-TAKEN_SEAT = 'X'
--- week8/The-Last-HR/settings.py
+++
@@ -1,4 +0,0 @@
-DB_SQL_FILE = "create_hackbulgaria_db.sql"
-DB_NAME = "hackbulgaria.db"
-# api at this url is not available
-URL = "https://hackbulgaria.com/api/students/"
